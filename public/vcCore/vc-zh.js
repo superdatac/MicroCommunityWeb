ca44a66e2166553c1b3b040fa05e109ad793f164
Gao Minglun <gaominglun@gmail.com>
2021-04-08 12:20:17 +0800

    fix

--- public/vcCore/vc-zh.js
+++ public/vcCore/vc-zh.js
@@ -4,16 +4,16 @@
 (function (window) {
     window.lang = {
         "systemName": "小蓝人小区管理系统",
-        "systemSimpleName": "HC",
+        "systemSimpleName": "小蓝人",
         "subSystemName": "小区管理系统",
-        "companyTeam": "java110 团队",
+        "companyTeam": "小蓝人科技",
         "welcome": "欢迎访问小蓝人小区管理系统",
         "signOut": "退出",
         "signIn":"登录",
         "register":"注册",
         "moreCommunity":"更多小区",
         "moreMsg":"查看全部消息",
-        "title":"小蓝人小区管理系统|java110",
+        "title":"小蓝人小区管理系统|小蓝人科技",
         "noAccount":"还没有账号？ ",
         "areyouhasaccount":"已经有账户了？" 
     }
